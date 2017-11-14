2b582a13aa3365582d80130dfacba87787f94834
Pedromrv <pedromrv@gmail.com>
2017-11-14 11:06:09 +0100

    Saving

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>CSS Layouts</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<div class="main">
+    <div class="main-header">
+    <div class="main-container">
+<div class="content"></div>
+<div class="content"></div>
+<div class="content"></div>
+<div class="content"></div>
+<div class="content"></div>
+</div>
+    </div>
+
+  <div class="next.container">
+  </div>
+
+  <div class="main-footer">©2017 ToDoList</div>
+</div>
+</body>
+
+</html>
