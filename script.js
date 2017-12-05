f843222d23015cf714c247985d1a340c3982a391
Pedro <32385095+Pedromrv@users.noreply.github.com>
2017-12-05 12:26:09 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+  $("#datepicker").datepicker();
+});
+
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+    $("#show").click(function(){
+        $("p").show();
+    });
+});
